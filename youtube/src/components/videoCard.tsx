fcf79b54e82dcacbe5afe79fda7e8debedd011cd
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2024-04-04 13:31:35 +0530

    videocard component is pushed

---
+++ youtube/src/components/videoCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function VideoCard(props: any) {
+  return (
+    <div>
+      <img src="pk.png" className="rounded-2xl"></img>
+      <div className="grid grid-cols-12 ">
+        <div className="col-span-1">
+          <img src="pk.png" className="w-13 h-12 rounded-full"></img>
+        </div>
+        <div className="col-span-11 pl-4">
+          <div> {props.title}</div>
+
+          <div className="text-gray-600 text-base">{props.author}</div>
+          <div className="text-gray-600 text-base">{props.views} </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoCard;
--- youtube/src/pages/index.tsx
+++ youtube/src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import VideoCard from "@/components/videoCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <VideoCard
+        title={"Prateek Kuhad - Mulaqat (Official Music Video) | Tara Sutaria"}
+        author={"Prateek Kuhad"}
+        views={"8.1M views"}
+      />
+    </div>
+  );
 }
